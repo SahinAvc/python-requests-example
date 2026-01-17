faf32b556dabcead12f983609560ae2370e940bb
Şahin Avcı <132500203+SahinAvc@users.noreply.github.com>
2026-01-17 16:33:30 +0300

    added requests example

---
+++ RequestsExample.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+#GET
+#user_input = input("Enter id: ")
+#url = f"https://jsonplaceholder.typicode.com/todos/{user_input}"
+
+#get_response = requests.get(url)
+#print(get_response.json())
+
+#POST
+to_do_item = {"userId" :2, "title" :"my to do", "completed" :False}
+post_url ="https://jsonplaceholder.typicode.com/todos"
+#optional header
+headers = {"Content-Type": "application/json"}
+#post_response = requests.post(post_url, json=to_do_item, headers=headers)
+post_response = requests.post(post_url, data=json.dumps(to_do_item), headers=headers)
+
+print(post_response.json())
+#print(json.dumps(to_do_item))
